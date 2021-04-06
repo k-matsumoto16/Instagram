54e503690b62ce26fb7935215ddf15f3fc80a491
kouki.matsumoto <koukimatsumoto16@gmail.com>
2021-04-06 21:12:47 +0900

    Instagram.xcworkspaceをコミット

---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        // タブバーの背景色
+        self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        // UITabBarControllerDelegateプロトコルのメソッド
+        self.delegate = self
+    }
+
+    // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッド
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is ImageSelectViewController {
+            // ImageSelectViewControllerは、タブ切り替えではなくモーダル画面遷移
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+            present(imageSelectViewController, animated: true)
+            return false
+        } else {
+            // その他のViewControllerは通常のタブ切り替え
+            return true
+        }
+    }
+
+}
